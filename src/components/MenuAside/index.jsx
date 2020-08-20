27a61bd735620fd5ca46a9995fc2ed0303cb68ee
Peppoderosa <ruan.victor21052000@gmail.com>
2020-08-20 20:49:27 -0300

    add dropdown test menu

--- src/components/MenuAside/index.jsx
+++ src/components/MenuAside/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 //assets
 import photo from "../../assets/images/ditto.png";
+import settingsIcon from "../../assets/icons/settings.svg";
+
 //css
 import "./styles.scss";
 //react-redux
@@ -10,6 +12,9 @@ import allActions from "../../store/actions";
 
 function MenuAside() {
   const dispatch = useDispatch();
+
+  const [showHentaiM, setShowHentaiM] = React.useState(false);
+  const [showEpisodeM, setShowEpisodeM] = React.useState(false);
 
   return (
     <aside className="menu-container">
@@ -21,6 +26,7 @@ function MenuAside() {
         className="section-button"
         onClick={() => dispatch(allActions.pageActions.changePage("home"))}
       >
+        <img className="icon-aside" src={settingsIcon} alt="" />
         Home
       </Link>
       <Link
@@ -28,6 +34,7 @@ function MenuAside() {
         className="section-button"
         onClick={() => dispatch(allActions.pageActions.changePage("settings"))}
       >
+        <img className="icon-aside" src={settingsIcon} alt="" />
         Configurações
       </Link>
       <Link
@@ -35,6 +42,7 @@ function MenuAside() {
         className="section-button"
         onClick={() => dispatch(allActions.pageActions.changePage("faplist"))}
       >
+        <img className="icon-aside" src={settingsIcon} alt="" />
         Lista de Fap
       </Link>
       <Link
@@ -42,6 +50,7 @@ function MenuAside() {
         className="section-button"
         onClick={() => dispatch(allActions.pageActions.changePage("status"))}
       >
+        <img className="icon-aside" src={settingsIcon} alt="" />
         Status Geral
       </Link>
       <Link
@@ -51,22 +60,48 @@ function MenuAside() {
           dispatch(allActions.pageActions.changePage("notifications"))
         }
       >
+        <img className="icon-aside" src={settingsIcon} alt="" />
         Notificações
       </Link>
       <Link
         to="/hentais"
         className="section-button"
-        onClick={() => dispatch(allActions.pageActions.changePage("hentais"))}
+        onClick={() =>
+          dispatch(
+            allActions.pageActions.changePage("hentais"),
+            setShowHentaiM(showHentaiM ? false : true)
+          )
+        }
       >
+        <img className="icon-aside" src={settingsIcon} alt="" />
         Hentais
       </Link>
+
+      {showHentaiM ? (
+        <>
+          <button className="sub-button">Todos Hentais</button>
+          <button className="sub-button">Cadastrar Hentais</button>
+        </>
+      ) : null}
       <Link
         to="/episodes"
         className="section-button"
-        onClick={() => dispatch(allActions.pageActions.changePage("episodes"))}
+        onClick={() =>
+          dispatch(
+            allActions.pageActions.changePage("episodes"),
+            setShowEpisodeM(showEpisodeM ? false : true)
+          )
+        }
       >
+        <img className="icon-aside" src={settingsIcon} alt="" />
         Episódios
       </Link>
+      {showEpisodeM ? (
+        <>
+          <button className="sub-button">Todos Episódios</button>
+          <button className="sub-button">Cadastrar Episódios</button>
+        </>
+      ) : null}
       <Link
         to="/taxonomies"
         className="section-button"
@@ -74,6 +109,7 @@ function MenuAside() {
           dispatch(allActions.pageActions.changePage("taxonomies"))
         }
       >
+        <img className="icon-aside" src={settingsIcon} alt="" />
         Taxonomias
       </Link>
       <Link
@@ -81,6 +117,7 @@ function MenuAside() {
         className="section-button"
         onClick={() => dispatch(allActions.pageActions.changePage("users"))}
       >
+        <img className="icon-aside" src={settingsIcon} alt="" />
         Usuários
       </Link>
       <Link
@@ -88,6 +125,7 @@ function MenuAside() {
         className="section-button"
         onClick={() => dispatch(allActions.pageActions.changePage("comments"))}
       >
+        <img className="icon-aside" src={settingsIcon} alt="" />
         Comentários
       </Link>
     </aside>
