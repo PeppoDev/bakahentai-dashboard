583b232af004896cc8ad88d3d1bf76c044795a6a
Peppoderosa <ruan.victor21052000@gmail.com>
2020-08-21 23:18:38 -0300

    add icons

--- src/components/MenuAside/index.jsx
+++ src/components/MenuAside/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 //assets
 import photo from "../../assets/images/ditto.png";
-import settingsIcon from "../../assets/icons/settings.svg";
+import settingsIcon from "../../assets/icons/config.svg";
+import commentsIcon from "../../assets/icons/comments.svg";
+import dashboardIcon from "../../assets/icons/dashboard.svg";
+import fappedIcon from "../../assets/icons/fapped.svg";
+import taxonomiesIcon from "../../assets/icons/taxonomias.svg";
+import notificationsIcon from "../../assets/icons/notifications.svg";
+import usersIcon from "../../assets/icons/users.svg";
+
 //css
 import "./styles.scss";
 //components
@@ -16,11 +23,15 @@ function MenuAside() {
       <h2>Peppa</h2>
       <p>Membro desde 17/08/2020</p>
 
-      <MenuButton to="home" initial icon={settingsIcon} text="Home" />
+      <MenuButton to="home" initial icon={dashboardIcon} text="Home" />
       <MenuButton to="settings" icon={settingsIcon} text="Configurações" />
-      <MenuButton to="faplist" icon={settingsIcon} text="Lista de Fap" />
-      <MenuButton to="status" icon={settingsIcon} text="Status Geral " />
-      <MenuButton to="notifications" icon={settingsIcon} text="Notificações" />
+      <MenuButton to="faplist" icon={fappedIcon} text="Lista de Fap" />
+      <MenuButton to="status" icon={settingsIcon} text="Status Geral" />
+      <MenuButton
+        to="notifications"
+        icon={notificationsIcon}
+        text="Notificações"
+      />
 
       <DropDownMenu text="Hentais" icon={settingsIcon}>
         <DropDownButton text="Todos Hentais" to="allhentais" />
@@ -32,11 +43,11 @@ function MenuAside() {
         <DropDownButton text="Cadastrar Episódios" to="registerepisodes" />
       </DropDownMenu>
 
-      <MenuButton to="taxonomies" icon={settingsIcon} text="Taxonomias" />
+      <MenuButton to="taxonomies" icon={taxonomiesIcon} text="Taxonomias" />
 
-      <MenuButton to="users" icon={settingsIcon} text="Usuários" />
+      <MenuButton to="users" icon={usersIcon} text="Usuários" />
 
-      <MenuButton to="comments" icon={settingsIcon} text="Comentários" />
+      <MenuButton to="comments" icon={commentsIcon} text="Comentários" />
     </aside>
   );
 }
