7d09d382de4c72ec4727bb555654956ff8bb170d
Peppoderosa <ruan.victor21052000@gmail.com>
2020-09-08 14:31:36 -0300

    add functions on image container at register episode

--- src/components/EpisodeItem/index.jsx
+++ src/components/EpisodeItem/index.jsx
@@ -10,8 +10,38 @@ import playIcon from "../../assets/icons/play.svg";
 
 export default function EpisodeItem({ id, name }) {
   const [showItens, setShowItens] = React.useState(true);
+  const [file, setFile] = React.useState("");
 
-  //TODO:resovlver a bagaça do MainInput disable
+  const fileInputRef = React.useRef();
+
+  const divStyle =
+    file !== ""
+      ? {
+          backgroundImage: "url(" + file + ")",
+        }
+      : null;
+
+  function handleDelete() {
+    setFile("");
+  }
+
+  function handleClickInput() {
+    fileInputRef.current.click();
+  }
+
+  function handleReplace() {
+    if (fileInputRef.current.files.length) {
+      if (fileInputRef.current.files[0].type.includes("image")) {
+        const reader = new FileReader();
+        reader.readAsDataURL(fileInputRef.current.files[0]);
+        reader.onload = function (e) {
+          setFile(reader.result);
+        };
+      } else {
+        console.log("Erro: formato não suportado");
+      }
+    }
+  }
   return (
     <article className="register-episodes-container">
       <div
@@ -27,6 +57,12 @@ export default function EpisodeItem({ id, name }) {
       {showItens ? (
         <>
           <article className="grid-episode-item">
+            <input
+              ref={fileInputRef}
+              className="file-input"
+              type="file"
+              onChange={handleReplace}
+            />
             <article className="main-left">
               <MainInput
                 valueName={name}
@@ -45,8 +81,27 @@ export default function EpisodeItem({ id, name }) {
                 placeholder="Ex. Algum anime"
               />
             </article>
-            <article className="main-right">
-              <DropZone text="Clique ou arraste uma imagem de até 1280x720" />
+            <article
+              className="main-right"
+              style={
+                file === ""
+                  ? { border: "2px dotted grey" }
+                  : { border: "2px dotted #2f2e2e" }
+              }
+            >
+              {file !== "" ? (
+                <div style={divStyle} className="image-container" alt="">
+                  <div className="button-group">
+                    <button onClick={handleClickInput}>Substituir</button>
+                    <button onClick={handleDelete}>Excluir</button>
+                  </div>
+                </div>
+              ) : (
+                <DropZone
+                  text="Clique ou arraste uma imagem de até 1280x720"
+                  setFiles={setFile}
+                />
+              )}
             </article>
           </article>
           <InputVideo />
