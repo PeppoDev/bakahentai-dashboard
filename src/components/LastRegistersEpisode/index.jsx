fba258794ed4024e17d06d61ba3d1db2279125d6
Peppoderosa <ruan.victor21052000@gmail.com>
2020-10-01 18:26:59 -0300

    fix some things

--- src/components/LastRegistersEpisode/index.jsx
+++ src/components/LastRegistersEpisode/index.jsx
@@ -8,7 +8,10 @@ export default function LastRegistersEpisode() {
   return (
     <article className="last-registers-list">
       <div className="registered-item">
-        <p>Nome do anime - Episode 1</p>
+        <span>
+          <p>Nome do anime</p>
+          <p>- Episode 1</p>
+        </span>
         <img src={penIcon} alt="" />
       </div>
       <div className="registered-item">
